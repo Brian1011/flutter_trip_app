7f7426d66d241bc76e473b6e7fcc223c0d4369ac
Brian Mutinda <mutindabrian26@gmail.com>
2024-01-11 15:22:49 +0300

    create model

--- lib/features/trip/data/models/trip_model.dart
+++ lib/features/trip/data/models/trip_model.dart
@@ -1,1 +1,44 @@
+import 'package:hive/hive.dart';
 
+import '../../domain/entities/trip.dart';
+
+part 'trip_model.g.dart';
+
+@HiveType(typeId: 0)
+class TripModel {
+  @HiveField(0)
+  final String title;
+  @HiveField(1)
+  final List<String> photos;
+  @HiveField(2)
+  final String description;
+  @HiveField(3)
+  final DateTime date;
+  @HiveField(4)
+  final String location;
+
+  TripModel(
+      {required this.title,
+      required this.photos,
+      required this.description,
+      required this.date,
+      required this.location});
+
+  // from entity to Model
+  factory TripModel.fromEntity(Trip trip) => TripModel(
+        title: trip.title,
+        photos: trip.photos,
+        description: trip.description,
+        date: trip.date,
+        location: trip.location,
+      );
+
+  // from model to entity
+  Trip toEntity() => Trip(
+        title: title,
+        date: date,
+        description: description,
+        location: location,
+        photos: photos,
+      );
+}
